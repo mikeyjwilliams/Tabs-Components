84ebfcae1afc16ff35f90e11a063f7b8baabd1ec
mikey <manicdesigner@gmail.com>
2019-05-09 20:28:11 -0400

    feat(rm tab-link-selected) link selected removed on click

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,10 +19,12 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
-    // console.log(links);
-    console.log('hi');
+    //console.log(links);
+    //console.log('hi');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links)
+    Array.from(links).forEach(link => {
+      link.classList.remove('tabs-link-selected');
+    })
 
     // Add a class named "tabs-link-selected" to this link
 
